3c1f14eeb39e797e1840519e304391fdec61acb5
AmweyInc <50361932+AmweyInc@users.noreply.github.com>
2022-04-05 16:59:03 +0300

    Finished jwt registration sample

--- env.js
+++
@@ -1,7 +0,0 @@
-const PORT = 7000
-const DB_URL = 'mongodb+srv://Amwey1337:root@cluster0.n26e8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
-module.export = {
-    PORT: PORT,
-    DB_URL: DB_URL
-};
